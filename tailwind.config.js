41fa3f9301ecaa05da996d8110f7c6e2eb411f82
Aman Kumar <17461@teamcomputers.com>
2024-07-03 09:19:27 +0530

    dark-light mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'selector',
   content: ["./**/*.{html,js}", 'node_modules/preline/dist/*.js'],
   theme: {
     extend: {
